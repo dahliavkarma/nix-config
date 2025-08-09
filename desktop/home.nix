641dacd347ca4e632ff44cd56daa733d768ecbb6
dahliavkarma <dahliavkarma@gmail.com>
2025-08-09 13:29:41 +0900

    added back gimp

--- desktop/home.nix
+++ desktop/home.nix
@@ -1,8 +1,9 @@
 {
   inputs,
-  pkgs, 
+  pkgs,
   ...
-}: {
+}:
+{
   imports = [
     ./mh-dev.nix
     ./mh-looks.nix
@@ -10,35 +11,40 @@
     ./mh-ocr.nix
     # ./mh-work.nix
   ];
-  home.packages = with pkgs; [
-    anki
-    calibre
-    discord
-    ffmpeg
-    # gimp
-    heroic
-    kdePackages.kate
-    kdePackages.okular
-    krita
-    libreoffice
-    motrix
-    musescore
-    scantailor-advanced
-    siyuan
-    telegram-desktop
-    thunderbird
-    tor-browser
-    tuxguitar
-    vscodium # remember to install Nix IDE, Catppuccin Icons, and the vsix file
-    yt-dlp
-    zotero
-  ] ++ (with inputs; [
-    zen-browser.packages.${pkgs.system}.default
-  ]);
-  programs.chromium = { # since chrome is needed from time to time
+  home.packages =
+    with pkgs;
+    [
+      anki
+      calibre
+      discord
+      ffmpeg
+      gimp
+      heroic
+      kdePackages.kate
+      kdePackages.okular
+      krita
+      libreoffice
+      motrix
+      musescore
+      scantailor-advanced
+      siyuan
+      telegram-desktop
+      thunderbird
+      tor-browser
+      tuxguitar
+      vscodium # remember to install Nix IDE, Catppuccin Icons, and the vsix file
+      yt-dlp
+      zotero
+    ]
+    ++ (with inputs; [
+      zen-browser.packages.${pkgs.system}.default
+    ]);
+  programs.chromium = {
+    # since chrome is needed from time to time
     enable = true;
     package = pkgs.ungoogled-chromium;
-    extensions = [ # doesn't work just yet
+    extensions = [
+      # doesn't work just yet
       "gcbommkclmclpchllfjekcdonpmejbdp" # https everywhere
       "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock origin
     ];
