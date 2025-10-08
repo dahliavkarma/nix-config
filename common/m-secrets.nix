0ccbdf8d9e75d40ff7dc0fb06c31fc7f62240ed9
dahliavkarma <dahliavkarma@gmail.com>
2025-10-09 08:27:20 +0900

    fixed typo

--- common/m-secrets.nix
+++ common/m-secrets.nix
@@ -14,7 +14,7 @@
     validateSopsFiles = false;
 
     age = {
-      # generate age key from host ssh key if nonexistant
+      # generate age key from host ssh key if nonexistent
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
