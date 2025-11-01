1772c6450273dacb1d71f629eb42cbf07dc7ec2c
dahliavkarma <dahliavkarma@gmail.com>
2025-11-01 19:55:04 +0900

    added actual-server to homelab

---
+++ hosts/homelab/m-finance.nix
@@ -0,0 +1,17 @@
+{
+  ...
+}:
+{
+  service.immich = {
+    enable = true;
+    port = 3000; # the default
+    settings.hostname = "localhost";
+  };
+  services.caddy.virtualHosts."actual.silverside-chimera.ts.net" = {
+    extraConfig = ''
+      bind tailscale/actual
+      tailscale_auth
+      reverse_proxy localhost:3000
+    '';
+  };
+}
