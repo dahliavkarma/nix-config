3f182c213797a7f6a57a28f535bb361d7cf1bf98
dahliavkarma <dahliavkarma@gmail.com>
2026-09-21 17:37:14 +0900

    delete ssh matchblocks

--- common/mh-shell.nix
+++ common/mh-shell.nix
@@ -84,7 +84,7 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
+    settings = {
       homelab = {
         host = "homelab";
         hostname = "192.168.151.11";
