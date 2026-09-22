e39f4cfb953b49d84168496f040d627339493e2d
dahliavkarma <dahliavkarma@gmail.com>
2026-09-22 18:36:00 +0900

    remove systemd integration (conflicts with uwsm)

--- hosts/home-desktop/mh-de-hyprland.nix
+++ hosts/home-desktop/mh-de-hyprland.nix
@@ -15,6 +15,7 @@ in
   wayland.windowManager.hyprland = {
     enable = true;
     configType = "hyprlang";
+    systemd.enable = false;
     settings = {
       debug.disable_logs = false;
       # xwayland.force_zero_scaling = true; # steam might run pixelated when false but most X11 apps won't scale when true
