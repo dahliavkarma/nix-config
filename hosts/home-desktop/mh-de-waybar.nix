38eb294650e03d446f06738d76d8256b73923ca8
dahliavkarma <dahliavkarma@gmail.com>
2026-09-23 00:11:36 +0900

    add waybar temp glyph variants

--- hosts/home-desktop/mh-de-waybar.nix
+++ hosts/home-desktop/mh-de-waybar.nix
@@ -23,7 +23,7 @@
   temperature = {
     critical-threshold = 80;
     format = "{temperatureC}°C {icon}";
-    format-icons = ["" "" ""];
+    format-icons = ["" "" "" "" ""];
     format-critical = "{temperatureC}°C ";
     tooltip = false;
   };
